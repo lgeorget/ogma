0b8e0896804a825740c42e0fa2f90f85937f631f
Laurent Georget <laurent@lgeorget.eu>
2016-05-14 17:23:39 +0200

    Create a database with one user in the sample application

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE OverloadedStrings          #-}
+
 module Main where
 
+import           Control.Monad.IO.Class (liftIO)
 import qualified Ogma.Api            as Api
 import           Ogma.Api.Definition
+import           Ogma.Model.Model
+import           Data.Text           (Text)
 import           Servant
 import           Network.Wai
 import           Network.Wai.Middleware.Cors
 import           Network.Wai.Handler.Warp
+import           Database.Persist
+import           Database.Persist.TH
+import           Database.Persist.Quasi
+import           Database.Persist.Sqlite
+import qualified Data.Auth.Identity as Auth
 
 ogmaProxy :: Proxy OgmaApi
 ogmaProxy = Proxy
@@ -14,4 +24,8 @@ ogma :: Application
 ogma = simpleCors $ serve ogmaProxy Api.hello
 
 main :: IO ()
-main = run 8080 ogma
+main = do runSqlite "db.sqlite" $ do
+            runMigration migrateAll
+            runMigration Auth.migrateAuth
+            createNewUser "moi" "moi@m.oi"
+          liftIO $ run 8080 ogma
